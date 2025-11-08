52dffc3efe2ea4d58e676d682a5a8981c05e9f8e
Omar Zuniga <miguel_omar30@hotmail.com>
2025-11-07 20:40:53 -0600

    Background Jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { LogoutButton } from "./logout";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
-const Page = async () => {
-	await requireAuth();
+const Page = () => {
+	const trpc = useTRPC();
+	const queryClient = useQueryClient();
+	const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-	const data = await caller.getUsers();
+	const create = useMutation(
+		trpc.createWorkflow.mutationOptions({
+			onSuccess: () => {
+				queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+				toast.success("Workflow created");
+			},
+		})
+	);
 
 	return (
 		<div className="min-h-screen min-w-screen flex flex-col items-center justify-center gap-y-6">
 			Protected Server Component
 			<div>{JSON.stringify(data, null, 2)}</div>
+			<Button disabled={create.isPending} onClick={() => create.mutate()}>
+				Create Workflow
+			</Button>
 			<LogoutButton />
 		</div>
 	);
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,22 @@
 import { prisma } from "@/lib/prisma";
 import { createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
 
 export const appRouter = createTRPCRouter({
-	getUsers: protectedProcedure.query(({ ctx }) => {
-		return prisma.user.findMany({
-			where: {
-				id: ctx.auth.user.id,
+	getWorkflows: protectedProcedure.query(({ ctx }) => {
+		return prisma.workflow.findMany({});
+	}),
+	createWorkflow: protectedProcedure.mutation(async ({ ctx }) => {
+		await inngest.send({
+			name: "test/hello.world",
+			data: {
+				email: "test@test.com",
+			},
+		});
+
+		return prisma.workflow.create({
+			data: {
+				name: "test-workflow",
 			},
 		});
 	}),
