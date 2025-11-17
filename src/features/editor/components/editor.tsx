4321dc67c6a2ecfbacd476af25210295b83a120f
Omar Zuniga <miguel_omar30@hotmail.com>
2025-11-17 00:01:56 -0600

    Editor Setup

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -2,6 +2,26 @@
 
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+import {
+	Background,
+	Controls,
+	MiniMap,
+	Panel,
+	ReactFlow,
+	addEdge,
+	applyEdgeChanges,
+	applyNodeChanges,
+	type Connection,
+	type Edge,
+	type EdgeChange,
+	type Node,
+	type NodeChange,
+} from "@xyflow/react";
+import { useCallback, useState } from "react";
+
+import { nodeComponents } from "@/config/node-components";
+import "@xyflow/react/dist/style.css";
+import { AddNodeButton } from "./add-node-button";
 
 export const EditorLoading = () => {
 	return <LoadingView message="Loading editor..." />;
@@ -14,5 +34,40 @@ export const EditorError = () => {
 export const Editor = ({ workflowId }: { workflowId: string }) => {
 	const { data: workflow } = useSuspenseWorkflow(workflowId);
 
-	return <div>{JSON.stringify(workflow, null, 2)}</div>;
+	const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+	const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+
+	const onNodesChange = useCallback(
+		(changes: NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+		[]
+	);
+	const onEdgesChange = useCallback(
+		(changes: EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+		[]
+	);
+	const onConnect = useCallback(
+		(params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+		[]
+	);
+
+	return (
+		<div className="size-full">
+			<ReactFlow
+				nodes={nodes}
+				edges={edges}
+				onNodesChange={onNodesChange}
+				onEdgesChange={onEdgesChange}
+				onConnect={onConnect}
+				nodeTypes={nodeComponents}
+				fitView
+				proOptions={{ hideAttribution: true }}>
+				<Background />
+				<Controls />
+				<MiniMap />
+				<Panel position="top-right">
+					<AddNodeButton />
+				</Panel>
+			</ReactFlow>
+		</div>
+	);
 };
